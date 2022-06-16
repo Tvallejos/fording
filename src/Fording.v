f7f5833f04998ef44b2d3d0a37415919d80de1da
Tomas <tomas.vallejos12@gmail.com>
2022-06-16 14:32:38 +0200

    adding documentation

--- src/Fording.v
+++ src/Fording.v
@@ -9,7 +9,10 @@ Instance prime_tsl_ident : TslIdent := {| tsl_ident := fun id => id ^ "'" |}.
 Definition tEq ctx:= TemplateToPCUIC.trans ctx <% @eq %>.
 
 
-Fixpoint lookup_ctx_type (n : nat) (Γ : context) :=
+(*
+  Returns the type of the nth identifier in the context
+*)
+Fixpoint lookup_ctx_type (n : nat) (Γ : context) : term :=
   match n, Γ with
   | O, h :: t => let (_,_,ty) := h in ty
   | S n', h :: t => lookup_ctx_type n' t
@@ -18,9 +21,7 @@ Fixpoint lookup_ctx_type (n : nat) (Γ : context) :=
 
 
 (* 
-returns 
-the type of an index eqn abstraction,
-new context (with the new equalities)
+  Introduces an equality between the nth new parameter and the index (t)
 *)
 Definition abstract_eqns (Σ : PCUICProgram.global_env_ext_map) (Γ : context) (idx_num arity nnewvars : nat) (t : term) : term :=
   let gem := PCUICProgram.global_env_ext_map_global_env_map Σ in
@@ -29,20 +30,10 @@ Definition abstract_eqns (Σ : PCUICProgram.global_env_ext_map) (Γ : context) (
   eqn.
 
 
-Definition split_at_n {A : Type} (l : list A) (n : nat) : (list A * list A) :=
-  let args := firstn n l in
-  let params := skipn n l in
-  (params,args).
-
-
-Fixpoint mkTProds (vars : context) (t : term) :=
-  match vars with
-  | nil => t
-  | v :: vs => let (na,_,ty) := v in tProd na ty (mkTProds vs t)
-  end.
-
-
-Definition build_type (args : context) (nnewvars nparams nbodies body_num : nat) : term := 
+(*
+  It builds the type of a constructor given the parameters
+*)
+Definition build_type (args : context) (nparams nbodies body_num : nat) : term := 
   let nap := #|args| in
   let fix build_type (args : context) :=
     match args with
@@ -52,28 +43,41 @@ Definition build_type (args : context) (nnewvars nparams nbodies body_num : nat)
     end in
   build_type args.
 
-
-Definition lift_decl (n from : nat) (decl : context_decl ) : context_decl :=
+(*
+  Lifts a context declaration by the specification of lifting a term
+*)
+Definition lift_decl (n k : nat) (decl : context_decl ) : context_decl :=
   let (na, bdy, ty) := decl in 
-  {| decl_name := na; decl_body := bdy; decl_type := (lift n from ty)|}.
+  {| decl_name := na; decl_body := bdy; decl_type := (lift n k ty)|}.
 
   
-Fixpoint lift_ctx (n from : nat) (ctx: list context_decl) : list context_decl :=
+(*
+  Lifts a context by the specification of lifting a term
+*)
+Fixpoint lift_ctx (n k : nat) (ctx: list context_decl) : list context_decl :=
   match ctx with
   | nil => nil
-  | d :: ds => lift_decl n from d :: lift_ctx n (from+1) ds
+  | d :: ds => lift_decl n k d :: lift_ctx n (k+1) ds
   end.
 
 
+(*
+  Builds a new constructor based on a previous one by moving indexes to parameters
+*)
 Definition build_cstr (Σ : PCUICProgram.global_env_ext_map) (Γ : context) (nparams nbodies body_num iter : nat) (cstr : constructor_body) : constructor_body :=
-  let cstr_inds := (cstr_indices cstr) in
-  let nnewvars := #|cstr_inds| in
-  let ctx := (app (rev (lift_ctx nnewvars 0 (rev (cstr_args cstr)))) Γ) in
-  let inds := mapi (fun i ind => abstract_eqns Σ ctx i (cstr_arity cstr) nnewvars ind) cstr_inds in
-  let nanon := (mkBindAnn nAnon Relevant) in 
-  let new_ctx := fold_left (fun g eq=> g ,, vass nanon eq) inds ctx in 
-  let type := build_type (rev new_ctx) nnewvars nparams nbodies body_num in 
+  let cstr_inds := (cstr_indices cstr) in (* list of indexes *)
+  let nnewvars := #|cstr_inds| in (* add this amount of identifiers *)
+  let ctx := (app (rev (lift_ctx nnewvars 0 (rev (cstr_args cstr)))) Γ) in 
+  (* build the current context using the parameters and previous args, 
+     lift the context taking into account the equalities that are going to be added *)
+  let inds := mapi (fun i ind => abstract_eqns Σ ctx i (cstr_arity cstr) nnewvars ind) cstr_inds in 
+  (* compute the equalities of every index *)
+  let new_ctx := fold_left (fun g eq=> g ,, vass (mkBindAnn nAnon Relevant) eq) inds ctx in 
+  (* add the equalities to the context *)
+  let type := build_type (rev new_ctx) nparams nbodies body_num in 
+  (* build the new type of the constructor *)
   let new_arity :=cstr.(cstr_arity) + nnewvars in
+  (* compute the new arity *)
   {|
     cstr_name:= tsl_ident (cstr_name cstr) ;
     cstr_args := firstn new_arity new_ctx;
@@ -83,6 +87,9 @@ Definition build_cstr (Σ : PCUICProgram.global_env_ext_map) (Γ : context) (npa
   |}.
 
 
+(*
+  Gives a name to the new parameters
+*)
 Definition change_name (name : aname) : aname := 
   let (bind,relev) := name in
   let newName := "new_name_change_name" in
@@ -93,6 +100,9 @@ Definition change_name (name : aname) : aname :=
   {| binder_name := newBind ; binder_relevance := relev|}.
                 
 
+(*
+  Gives a name to every index in the term
+*)
 Fixpoint replace_anon_names (t : term) : term :=
   match t with 
   | tProd na A B => tProd (change_name na) A (replace_anon_names B)
@@ -100,6 +110,9 @@ Fixpoint replace_anon_names (t : term) : term :=
   end. 
 
 
+(*
+  Builds a new body based on a previous one by generalizing every constructor 
+*)
 Definition build_bodies (Σ : PCUICProgram.global_env_ext_map) (Γ : context) (bodies : list one_inductive_body)
  (i0 : nat) (nparams : nat) : list one_inductive_body :=
  let nbodies := #|bodies| in
@@ -108,7 +121,9 @@ Definition build_bodies (Σ : PCUICProgram.global_env_ext_map) (Γ : context) (b
           ind_name := tsl_ident (ind_name ind);
           ind_indices := [];
           ind_type  := replace_anon_names ind.(ind_type) ; 
+          (* give name to indexes *)
           ind_ctors :=  mapi (build_cstr Σ Γ nparams nbodies body_num) (ind_ctors ind) ;
+          (* build every constructor by inserting equations before every index *)
           (* just proj below *)
           ind_sort := ind.(ind_sort);
           ind_kelim := ind.(ind_kelim) ;
@@ -117,6 +132,9 @@ Definition build_bodies (Σ : PCUICProgram.global_env_ext_map) (Γ : context) (b
           bodies.
 
 
+(*
+  Gives a name to every index in the context, namely indexes
+*)
 Fixpoint give_names_to_anon (inds : context) : context :=
   match inds with
   | nil => nil
@@ -127,13 +145,19 @@ Fixpoint give_names_to_anon (inds : context) : context :=
   end.
 
 
+(*
+  Builds a new mutually inductive definition based on a previous one 
+  by fording each body 
+*)
 Definition build_mind (Σ : PCUICProgram.global_env_ext_map) (Γ : context) (mind : mutual_inductive_body) (ind0 : inductive): mutual_inductive_body
   := 
   let inds :=  (match (head (ind_bodies mind)) with None => [] | Some b =>  ind_indices b end) in
-  let inds' := give_names_to_anon inds in
-  let params' := app inds' mind.(ind_params) in
-  let nparams := mind.(ind_npars) + #|inds'| in
   (* here we assume that every inductive body has the same indexes *)
+  let inds' := give_names_to_anon inds in
+  (* gives a name to indexes *)
+  let params' := app inds' mind.(ind_params) in
+  (* parameters now include the indexes (now with name) *)
+  let nparams := mind.(ind_npars) + #|inds'| in
   let i0 := inductive_ind ind0 in
      {| ind_finite := mind.(ind_finite);
         ind_npars :=  nparams;
@@ -145,6 +169,9 @@ Definition build_mind (Σ : PCUICProgram.global_env_ext_map) (Γ : context) (min
       |}.
 
 
+(*
+  Builds a forded inductive definition by quoting the input
+*)
 Definition build_ind {A : Type} (x : A)
   : TemplateMonad unit
   := 
